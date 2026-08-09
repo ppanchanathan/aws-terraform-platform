29ffb217d80f0a31d7aa52a4df53eecbba51612f
Pasupathi Panchanathan <pasupathivet@gmail.com>
2026-08-09 01:15:42 -0500

    Initialize platform repository structure

---
+++ bootstrap/iam-target-roles.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "github_actions" {
+  name = "github-terraform-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:ppanchanathan/aws-terraform-platform:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
