442269680e7df56499b6f9499d5b44a024d67c31
Pasupathi Panchanathan <pasupathivet@gmail.com>
2026-08-09 02:55:52 -0500

    Add bootstrap infrastructure (#1)
    Co-authored-by: Pasupathi.Panchanathan <pasupathi.panchanathan@bp.com>

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,1 +1,36 @@
 # Add remote backend after first local bootstrap apply.
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket =   "ppanchanathan-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket =   aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket =   aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name =   "terraform-locks"
+  billing_mode =   "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -2,3 +2,7 @@ variable "aws_region" {
   type    = string
   default = "us-east-2"
 }
+
+variable "github_repository" {
+  default = "ppanchanathan/aws-terraform-platform"
+}
