5ef7fec04db312fab774568f54036f1375b46b69
Pasupathi.Panchanathan <pasupathi.panchanathan@bp.com>
2026-08-09 17:45:38 -0500

    Add networking modules

--- platform/environments/dev/main.tf
+++ platform/environments/dev/main.tf
@@ -6,3 +6,18 @@ module "vpc" {
   name = "dev-vpc"
   cidr = "10.0.0.0/16"
 }
+
+module "subnet" {
+  source = "../../modules/networking/subnets"
+
+  name   = "public-subnet"
+  cidr   = "10.0.1.0/24"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "routing" {
+  source = "../../modules/networking/routing"
+
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.subnet.public_subnet_id
+}
